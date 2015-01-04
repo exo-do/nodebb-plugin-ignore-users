c1d4232fd72eb00aef5c2fb72ed3936382add57a
xusoo <Josep@MacBook-de-Josep.local>
2015-01-04 23:29:11 +0100

    Fix indexOf MongoDB

--- library.js
+++ library.js
@@ -25,7 +25,7 @@ plugin.parsePosts = function (data, callback) {
 
 				data.posts.forEach(function (post) {
 					post.ignored = false;
-					if (uids.indexOf(post.uid) !== -1) {
+					if (indexOf(uids, post.uid) !== -1) {
 						post.originalContent = post.content;
 						post.content = "Este post está oculto porque <b>" + post.user.username + "</b> está en tu lista de ignorados.";
 						post.ignored = true;
@@ -55,5 +55,19 @@ Socket.ignoreUser = function (socket, data, callback) {
 Socket.unignoreUser = function (socket, data, callback) {
 	db.setRemove('ignored:' + socket.uid, data.ignoreduid, callback);
 };
+
+/**
+ * Implementación del indexOf que no compara estrictamente (===) ya que Redis devuelve los uids en forma [1, 2, 3] y mongo ['1', '2', '3'], de esta forma, soportamos ambos.
+ */
+function indexOf(array, element) {
+	var index = -1;
+	array.forEach(function (el, i) {
+		if (el == element) {
+			index = i;
+			return false;
+		}
+	});
+	return index;
+}
 
 module.exports = plugin;
