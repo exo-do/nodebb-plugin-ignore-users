e2fbfdc4f72578bc11af4ee52c5176c6eebf3345
Pixelatedbug <pixeldot0@gmail.com>
2015-11-06 09:29:00 +0100

    Update profile.js

--- static/lib/profile.js
+++ static/lib/profile.js
@@ -1,39 +1,39 @@
 (function () {
-	"use strict";
-	
-	$(window).on('action:ajaxify.contentLoaded', function (event, data) {
-		/* Si estamos en la vista de usuarios ignorados */
-		if (data.url.match(/^user\/.+\/ignored$/i)) {
-			$('.users.account.ignored-users').on('click', 'button.unignore', function () {
-				var userbox = $(this).parents('.users-box');
-				unignoreUser({id: userbox.data('uid'), name: userbox.find('.username').text()}, function () {
-					userbox.fadeOut();
-				});
-			});
-		}
-	});
+    "use strict";
+    
+    $(window).on('action:ajaxify.contentLoaded', function (event, data) {
+        /* If we are in the ignored-users screen*/
+        if (data.url.match(/^user\/.+\/ignored$/i)) {
+            $('.users.account.ignored-users').on('click', 'button.unignore', function () {
+                var userbox = $(this).parents('.users-box');
+                unignoreUser({id: userbox.data('uid'), name: userbox.find('.username').text()}, function () {
+                    userbox.fadeOut();
+                });
+            });
+        }
+    });
 
 
-	/**
-	 * Des-ignora al usuario seleccionado
-	 */
-	function unignoreUser(user, callback) {
-		socket.emit('modules.unignoreUser', {ignoreduid: user.id}, function (err, res) {
-			
-			if (err) {
-				return;
-			}
+    /**
+     * Unignore selected user
+     */
+    function unignoreUser(user, callback) {
+        socket.emit('modules.unignoreUser', {ignoreduid: user.id}, function (err, res) {
+            
+            if (err) {
+                return;
+            }
 
-			callback();
-			
-			//Mostramos un aviso al usuario
-			app.alert({
-				message: 'A partir de ahora volver&aacute;s a ver todos los posts de <b>' + user.name + '</b>',
-				type: 'success',
-				timeout: 5000
-			});
-			
-		});
-	}
-	
+            callback();
+            
+            //Show the user a warning
+            app.alert({
+                message: 'From now on <b>' + user.name + '</b>’s publications will be visible',
+                type: 'success',
+                timeout: 5000
+            });
+            
+        });
+    }
+    
 }());
